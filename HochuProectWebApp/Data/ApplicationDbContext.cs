7438b7a43b9f40244f1f25c6eb0f94225fa4d413
Semen <semen.ruman@gmail.com>
2026-04-01 17:05:36 +0300

    Установил необходимые NuGet пакеты для работы с EF core и PostgreSql. Описал класс ApplicationDbContext и добавил туда все настройки моделей

---
+++ HochuProectWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore;
+using HochuProectWebApp.Models;
+
+namespace HochuProectWebApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Advertisement> Advertisements { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connectionString = config.GetConnectionString("PostgresConnection");
+
+            optionsBuilder.UseNpgsql(connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Advertisement>()
+                .HasOne(a => a.User)
+                .WithMany(u => u.Advertisements)
+                .HasForeignKey(a => a.UserId)
+                .OnDelete(DeleteBehavior.Cascade); 
+
+            modelBuilder.Entity<Advertisement>()
+                .HasOne(a => a.Category)
+                .WithMany(c => c.Advertisements)
+                .HasForeignKey(a => a.CategoryId)
+                .OnDelete(DeleteBehavior.Restrict); 
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(e => e.Name).HasMaxLength(100);
+                entity.Property(e => e.Email).HasMaxLength(100);
+                entity.HasIndex(e => e.Email).IsUnique(); 
+            });
+
+            modelBuilder.Entity<Category>(entity =>
+            {
+                entity.Property(e => e.Name).HasMaxLength(50).IsRequired();
+                entity.HasIndex(e => e.Name).IsUnique();
+            });
+
+            modelBuilder.Entity<Advertisement>(entity =>
+            {
+                entity.Property(e => e.Title).HasMaxLength(200).IsRequired();
+                entity.Property(e => e.Description).HasMaxLength(3000);
+            });
+
+            //найстройка сохранения списка фотографий для postgre sql
+            modelBuilder.Entity<Advertisement>(entity =>
+            {
+                entity.Property(e => e.Title).HasMaxLength(200).IsRequired();
+                entity.Property(e => e.Description).HasMaxLength(3000);
+
+                entity.Property(e => e.Photos)
+                    .HasColumnType("jsonb") 
+                    .HasDefaultValue("[]"); 
+            });
+        }
+    }
+}
