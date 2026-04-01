2f5d150f3ad11a5955b3416f403164308d086646
Semen <semen.ruman@gmail.com>
2026-04-01 16:33:06 +0300

    Добавил класс Advertisement для описания объявления

---
+++ HochuProectWebApp/Models/Advertisement.cs
@@ -0,0 +1,13 @@
+﻿namespace HochuProectWebApp.Models
+{
+    public class Advertisement
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public List<string> Photos {  get; set; } = new List<string>();
+
+        public string Description { get; set; } = "Нет описания";
+    }
+}
