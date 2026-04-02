3143d6337957b49cdcb2157ebb6beaa8417f73e1
Semen <semen.ruman@gmail.com>
2026-04-03 02:27:49 +0300

    Добавил ICategoryService для описания работы с БД категорий

---
+++ HochuProectWebApp/Services/Interfaces/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using HochuProectWebApp.Models;
+
+namespace HochuProectWebApp.Services.Interfaces
+{
+    public interface ICategoryService
+    {
+        public List<Category> GetCategories();
+
+        public bool AddCategory(Category category);
+
+        public bool RemoveCategory(int categoryId);
+    }
+}
