d19ca78ae6c984508629007cdd4c359309ee124a
Semen <semen.ruman@gmail.com>
2026-04-03 02:36:35 +0300

    Добавил CategoryEfService для реализации интерфейса ICategoryService

---
+++ HochuProectWebApp/Services/EF core/CategoryEfService.cs
@@ -0,0 +1,50 @@
+﻿using HochuProectWebApp.Data;
+using HochuProectWebApp.Models;
+using HochuProectWebApp.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace HochuProectWebApp.Services.EF_core
+{
+    public class CategoryEfService : ICategoryService
+    {
+        public List<Category> GetCategories()
+        {
+            using var dbContext = new ApplicationDbContext();
+
+            return dbContext.Categories.AsNoTracking().ToList();
+        }
+
+        public bool AddCategory(Category category)
+        {
+            using var dbContext = new ApplicationDbContext();
+
+            if (dbContext.Categories.Select(c => c.Name).Contains(category.Name))
+            {
+                return false;
+            }
+            else
+            {
+                dbContext.Categories.Add(category);
+                dbContext.SaveChanges();
+                return true;
+            }
+        }
+
+        public bool RemoveCategory(int categoryId)
+        {
+            using var dbContext = new ApplicationDbContext();
+
+            var category = dbContext.Categories.Find(categoryId);
+
+            if (category == null)
+            {
+                return false;
+            }
+            else
+            {
+                dbContext.Categories.Remove(category);
+                return true;
+            }
+        }
+    }
+}
