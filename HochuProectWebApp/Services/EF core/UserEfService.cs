08477327dcf575304102f4a3fd7bc41fb556d67e
Semen <semen.ruman@gmail.com>
2026-04-03 02:45:05 +0300

    Добавил UserEfService для реализации интерфейса IUserService

---
+++ HochuProectWebApp/Services/EF core/UserEfService.cs
@@ -0,0 +1,54 @@
+﻿using HochuProectWebApp.Data;
+using HochuProectWebApp.Models;
+using HochuProectWebApp.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace HochuProectWebApp.Services.EF_core
+{
+    public class UserEfService : IUserService
+    {
+        public User GetUserById(int userId)
+        {
+            using var dbContext = new ApplicationDbContext();
+            return dbContext.Users.Find(userId);
+        }
+
+        public User GetUserByEmail(string email)
+        {
+            using var dbContext = new ApplicationDbContext();
+            return dbContext.Users.AsNoTracking().FirstOrDefault(u => u.Email == email);
+        }
+
+        public bool AddUser(User user)
+        {
+            using var dbContext = new ApplicationDbContext();
+            if (dbContext.Users.Select(u => u.Email).Contains(user.Email))
+            {
+                return false;
+            }
+            else
+            {
+                dbContext.Users.Add(user);
+                dbContext.SaveChanges();
+                return true;
+            }
+        }
+
+        public bool RemoveUser(int userId)
+        {
+            using var dbContext = new ApplicationDbContext();
+            var user = dbContext.Users.Find(userId);
+
+            if (user == null)
+            {
+                return false;
+            }
+            else
+            {
+                dbContext.Remove(user);
+                dbContext.SaveChanges();
+                return true;
+            }
+        }
+    }
+}
