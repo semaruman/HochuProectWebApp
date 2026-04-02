5c24dec5dfd9fa83fc61dacb270b2879ffbd211c
Semen <semen.ruman@gmail.com>
2026-04-03 02:31:11 +0300

    Добавил IUserService для описания работы с БД пользователей

---
+++ HochuProectWebApp/Services/Interfaces/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using HochuProectWebApp.Models;
+
+namespace HochuProectWebApp.Services.Interfaces
+{
+    public interface IUserService
+    {
+        public User GetUserById(int userId);
+
+        public User GetUserByEmail(string email);
+
+        public bool AddUser(User user);
+
+        public bool RemoveUser(int userId);
+    }
+}
