ed2cea19c941b98cea5a5eab92cb5e5178fbfb3c
Semen <semen.ruman@gmail.com>
2026-04-04 06:46:49 +0300

    Добавил вспомогательный класс RegisterDto для регистрации пользователя

---
+++ HochuProectWebApp/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HochuProectWebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        /*
+        [HttpPost("register")]
+        public IActionResult RegisterUser()
+        {
+
+        }
+        */
+    }
+}
---
+++ HochuProectWebApp/DTOs/User/RegisterDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HochuProectWebApp.DTOs.User
+{
+    public class RegisterDto
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; }
+    }
+}
