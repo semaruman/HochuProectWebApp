30aed53b1ca52d302b341ca988dc9cc7d4016c59
Semen <semen.ruman@gmail.com>
2026-04-04 06:14:21 +0300

    Подключил футентификацию и авторизацию в Program.cs

--- HochuProectWebApp/Program.cs
+++ HochuProectWebApp/Program.cs
@@ -1,6 +1,7 @@
 
 using HochuProectWebApp.Services.EF_core;
 using HochuProectWebApp.Services.Interfaces;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Http.HttpResults;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,9 +10,14 @@ builder.Services.AddControllers();
 builder.Services.AddScoped<IAdvertisementService, AdvertisementEfService>();
 builder.Services.AddScoped<IUserService, UserEfService>();
 builder.Services.AddScoped<ICategoryService, CategoryEfService>();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options => options.LoginPath = "/login");
 
 var app = builder.Build();
+app.UseAuthentication();
+app.UseAuthorization();
 
+app.MapGet("/", GetMenu);
 app.Run();
 
 IResult GetMenu()
