cc4f9251268a6d7a48624f9c7b29bd1ed360dd5b
Semen <semaruman@yandex.ru>
2026-05-31 09:35:40 +0300

    Добавил интерфейс IRepository и его реализацию для применения паттерна репозиторий

---
+++ HochuProectWebApp/Data/Repositories/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace HochuProectWebApp.Data.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T?> GetByIdAsync(int id);
+        Task<HashSet<T>> GetAllAsync();
+        Task<HashSet<T>> FindAsync(Expression<Func<T, bool>> predicate);
+        Task<T?> FirstOrDefaultAsync(Expression<Func<T, bool>> predicate);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> predicate);
+        void Add(T entity);
+        void Update(T entity);
+        void Remove(T entity);
+        Task<int> SaveChangesAsync();
+    }
+}
