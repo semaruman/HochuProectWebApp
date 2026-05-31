c8806780acfcb78014a82cc38186a1a04dde6c14
Semen <semaruman@yandex.ru>
2026-05-31 09:49:03 +0300

    добавил класс UnitOfWork для реализации этого паттерна

---
+++ HochuProectWebApp/Data/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,58 @@
+﻿using HochuProectWebApp.Data.Repositories;
+using HochuProectWebApp.Models;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace HochuProectWebApp.Data.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+        private IDbContextTransaction? _transaction;
+
+        public IRepository<User> Users { get; }
+
+        public IRepository<Category> Categories { get; }
+
+        public IRepository<Advertisement> Advertisements { get; }
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+            Users = new Repository<User>(_context);
+            Categories = new Repository<Category>(_context);
+            Advertisements = new Repository<Advertisement>(_context);
+        }
+
+        public async Task<int> SavaChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public async Task BeginTransactionAsync()
+        {
+            _transaction = await _context.Database.BeginTransactionAsync();
+        }
+
+        public async Task CommitTransactionAsync()
+        {
+            if (_transaction != null)
+            {
+                await _transaction.CommitAsync();
+            }
+        }
+
+        public async Task RollbackTransactionAsync()
+        {
+            if (_transaction != null)
+            {
+                await _transaction.RollbackAsync();
+            }
+        }
+
+        public void Dispose()
+        {
+            _transaction?.Dispose();
+            _context.Dispose();
+        }
+    }
+}
