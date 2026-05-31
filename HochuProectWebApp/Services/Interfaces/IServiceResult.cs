5a411e8689b9a2a470caeb280a4bca9a4c753a17
Semen <semaruman@yandex.ru>
2026-05-31 10:07:07 +0300

    Реализовал Result Object Pattern для сервисов

---
+++ HochuProectWebApp/Services/Interfaces/IServiceResult.cs
@@ -0,0 +1,10 @@
+﻿namespace HochuProectWebApp.Services.Interfaces
+{
+    public interface IServiceResult<T>
+    {
+        bool IsSuccess { get; }
+        T? Value { get; }
+        string? ErrorMessage { get; }
+        int StatusCode { get; }
+    }
+}
