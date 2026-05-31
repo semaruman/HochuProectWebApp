fdba4ab4336048bb73e2227b4536ab289ac0ab7f
Semen <semaruman@yandex.ru>
2026-05-31 09:40:24 +0300

    Добавил интерфейс IUnitOfWork для реализации паттерна

---
+++ HochuProectWebApp/Data/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using HochuProectWebApp.Data.Repositories;
+using HochuProectWebApp.Models;
+
+namespace HochuProectWebApp.Data.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<User> Users { get; }
+
+        IRepository<Category> Categories { get; }
+
+        IRepository<Advertisement> Advertisements { get; }
+
+        Task<int> SavaChangesAsync();
+
+        Task BeginTransactionAsync();
+
+        Task CommitTransactionAsync();
+
+        Task RollbackTransactionAsync();
+    }
+}
